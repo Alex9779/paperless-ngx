ad996369768522329a0b4e40c94f4c81e5532ab3
Alexander Leisentritt <alexander.leisentritt@alitecs.de>
2026-01-22 18:38:52 +0100

    Fixes api app config test

--- src/documents/tests/test_api_app_config.py
+++ src/documents/tests/test_api_app_config.py
@@ -66,6 +66,9 @@ class TestApiAppConfig(DirectoriesMixin, APITestCase):
                 "barcode_max_pages": None,
                 "barcode_enable_tag": None,
                 "barcode_tag_mapping": None,
+                "barcode_enable_metadata": None,
+                "barcode_metadata_mapping": None,
+                "barcode_metadata_auto_create": None,
             },
         )
 
